51d0a7a922192cb7bf74fb04a29c1eff18d44598
harival120494@gmail.com <harival120494@gmail.com>
2022-08-03 22:04:50 +0700

    List Repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Container, Form } from "react-bootstrap";
+import Repo from "./Components/repo/index";
 
-function App() {
+const GithubRepo = () => {
+  const [repos, setRepos] = useState([]);
+
+  const handleChange = (e) => {
+    const repoUrl = `https://api.github.com/users/${e.target.value}/repos`;
+    axios
+      .get(repoUrl)
+      .then((responses) => {
+        const reposTemp = responses.data.map(
+          ({ name, language, html_url, created_at, description }) => {
+            return { name, language, html_url, created_at, description };
+          }
+        );
+        setRepos(reposTemp);
+        console.log(e.target.value);
+      })
+      .catch((error) => {
+        console.log(`inside getrepos error: ${error}`);
+        this.setState({
+          errorMessage: error.response.statusText,
+        });
+      });
+  };
+
+  const ShowRepos = () => {
+    return repos.map((repo) => <Repo key={repo.name} repo={repo} />);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Form className="mt-3">
+        <Form.Group className="mb-3">
+          <Form.Label>Github Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Masukan username github anda"
+            onChange={handleChange}
+          />
+        </Form.Group>
+      </Form>
+      <h5>List Repository : </h5>
+      {repos.length > 0 && <ShowRepos />}
+    </Container>
   );
-}
+};
 
-export default App;
+export default GithubRepo;
